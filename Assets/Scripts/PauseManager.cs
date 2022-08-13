39d19c44c192ea5e1572718745b57b50be238650
KaNaBiNoL <tassadar1624@gmail.com>
2022-08-13 19:13:56 +0300

    Add more UI (pausePanel + logic)

--- Assets/Scripts/PauseManager.cs
+++ Assets/Scripts/PauseManager.cs
@@ -1,35 +1,77 @@
 using System;
+using TMPro;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class PauseManager : SingletonMonoBehaviour<PauseManager>
 {
-   #region Variables
+    #region Variables
 
-   public bool _isPaused;
+    [SerializeField] private GameObject _pausePanel;
 
-   #endregion
+    [SerializeField] private Button ContinueButton;
+    [SerializeField] private Button RestartButton;
+    [SerializeField] private Button ExitButton;
 
+    public bool _isPaused;
 
-   #region Unity lifecycle
-
-   private void Update()
-   {
-      if (Input.GetKeyDown(KeyCode.Space))
-      {
-         TogglePause();
-      }
-   }
-
-   #endregion
+    #endregion
 
 
-   #region Public methods
+    #region Unity lifecycle
 
-   public void TogglePause()
-   {
-      _isPaused = !_isPaused;
-      Time.timeScale = _isPaused ? 0 : 1;
-   }
+    private void Start()
+    {
+        ContinueButton.onClick.AddListener(ContinueButtonClicked);
+        RestartButton.onClick.AddListener(RestartButtonClicked);
+        ExitButton.onClick.AddListener(ExitButtonClicked);
+    }
 
-   #endregion
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && _pausePanel.activeSelf == false)
+        {
+            TogglePause();
+            _pausePanel.SetActive(true);
+            
+        }
+    }
+
+    #endregion
+
+
+    #region Public methods
+
+    public void TogglePause()
+    {
+        
+        _isPaused = !_isPaused;
+        Time.timeScale = _isPaused ? 0 : 1;
+    }
+
+    #endregion
+
+
+    #region Private methods
+
+    private void RestartButtonClicked()
+    {
+        
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        TogglePause();
+    }
+
+    private void ContinueButtonClicked()
+    {
+        TogglePause();
+        _pausePanel.SetActive(false);
+    }
+
+    private void ExitButtonClicked()
+    {
+        Application.Quit();
+    }
+
+    #endregion
 }
